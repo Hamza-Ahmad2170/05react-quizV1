b836e3b94ea8c287f849b57ae2088b0556d961e7
hasaigal <hasaigal@gmail.com>
2024-02-04 15:35:53 +0500

    react

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '05react-quizV1',
   server: {
     open: true,
   },
